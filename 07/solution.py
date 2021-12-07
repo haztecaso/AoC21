256c7dfd8260beeb29b438ca2a4bbc259f5960d7
haztecaso <adrianlattes@disroot.org>
2021-12-07 23:16:34 +0100

    day 7 part 2

--- 07/solution.py
+++ 07/solution.py
@@ -2,11 +2,23 @@
 
 from functools import reduce
 
-def align_to_position(reference, positions):
+def align_to_position_v1(reference, positions):
     return reduce(lambda x, y: x+y,
             map(lambda position: abs(reference-position), positions)
            )
 
+def fuel_distance(a,b): # (triangular sum)
+    n = abs(a-b)
+    # print(f"{(a,b)=}; {n=}; {n*(n+1)/2=}")
+    return n*(n+1)/2
+
+
+def align_to_position_v2(reference, positions):
+    result = reduce(lambda x, y: x+y,
+            map(lambda position: fuel_distance(reference, position), positions)
+           )
+    # print(f"{reference = } {result = }")
+    return result
 
 def parse_input(filename):
     with open(filename, "r") as f:
@@ -16,8 +28,14 @@ def parse_input(filename):
 
 
 def part1(positions):
-    all = map(lambda position: align_to_position(position, positions), positions)
-    print(min(*all))
+    all = map(lambda position: align_to_position_v1(position, positions), positions)
+    print("Result part 1:", min(*all))
+
+def part2(positions):
+    references = range(min(positions), max(positions)+1)
+    all = map(lambda ref: align_to_position_v2(ref, positions), references)
+    print("Result part 2:", int(min(*all)))
 
 positions = parse_input("input")
 part1(positions)
+part2(positions)
