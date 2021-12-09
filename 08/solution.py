c8ed52231bb4be666420a03c41a148e16d038ec0
haztecaso <adrianlattes@disroot.org>
2021-12-09 13:48:57 +0100

    ignore sample files and add solution template

--- 08/solution.py
+++ 08/solution.py
@@ -1,44 +1,145 @@
 #!/usr/bin/env python3
-from dataclasses import dataclass
 
-BOLD = '\033[1m'
-END = '\033[0m'
+from itertools import groupby
+from functools import reduce
+from copy import deepcopy
 
-def bold(string):
-    return f"{BOLD}{string}{END}"
+digitsegments = { 0: "ABCEFG", 1: "CF", 2: "ACDEG", 3: "ACDFG", 4: "BCDF", 5: "ABDFG", 6: "ABDEFG", 7: "ACF", 8: "ABCDEFG", 9: "ABCDFG" }
+len2segments = {
+        n: reduce(lambda a, b: a.union(b),
+            map(lambda a: set(a),
+                filter(lambda v: len(v) == n, digitsegments.values())
+                )
+            )
+        if any(len(v) == n for v in  digitsegments.values())
+        else set()
+        for n in range(8)
+        }
 
-class Pattern():
-    def __init__(self, str_pattern):
-        self.letters = str_pattern.strip()
-        self.marked = False
+def str_intersection(a, b):
+    return set(a).intersection(b)
+
+class Inclusion():
+    def __init__(self, letter, segments):
+        self.letter = letter
+        self.segments:set = segments
 
     @property
-    def len(self):
-        return len(self.letters)
+    def is_equality(self):
+        return len(self.segments)==1
+    
+    @property
+    def possible_eqs(self):
+        result = []
+        for segment in self.segments:
+            return [Inclusion(self.letter, {segment}) for segment in self.segments]
 
     def __repr__(self):
-        return bold(self.letters) if self.marked else self.letters
+        if self.is_equality:
+            return f"{self.letter} = {self.segments.copy().pop()}"
+        else:
+            return f"{self.letter} ∈ {self.segments}"
+
+class InclusionSet():
+    def __init__(self, inclusions):
+        self.inclusions = inclusions
+
+    def simplify(self):
+        self.merge_inclusions()
+        for _ in range(8):
+            self.enforce_equalities()
+
+    def merge_inclusions(self):
+        self.inclusions.sort(key=lambda inc: inc.letter)
+        new_inclusions = []
+        for letter, group in groupby(self.inclusions, lambda inc: inc.letter):
+            segment_list = list(map(lambda inc: inc.segments, group))
+            segments = reduce(lambda a, b: a.intersection(b), segment_list)
+            new_inclusions.append(Inclusion(letter,segments)) 
+        self.inclusions = new_inclusions
+
+    def insert_eq(self, eq):
+        assert eq.is_equality
+        self.inclusions.append(eq)
+
+    @property
+    def possible_eqs(self):
+        result = []
+        for inclusion in filter(lambda i: not i.is_equality, self.inclusions):
+            result += inclusion.possible_eqs
+        return result
+
+    def enforce_equalities(self):
+        for eq in filter(lambda i: i.is_equality, self.inclusions):
+            letter = eq.letter
+            self.inclusions = list(map(lambda inclusion: inclusion if inclusion.is_equality
+                    else Inclusion(inclusion.letter, inclusion.segments.difference(eq.segments))
+                        , self.inclusions))
+
+    def __add__(self, other):
+        result = InclusionSet(self.inclusions + other.inclusions)
+        return result
+    
+    def __repr__(self):
+        return "\n".join([str(inclusion) for inclusion in self.inclusions])
 
 
-def parse_patterns(raw_str):
-    return [Pattern(p) for p in raw_str.split()]
+class Pattern():
+    def __init__(self, raw_str):
+        self.letters = raw_str.strip()
+        self._inclusions = None
+
+    @property
+    def len(self): return len(self.letters)
+
+    @property
+    def inclusions(self):
+        if not self._inclusions: 
+            self._inclusions = InclusionSet([Inclusion(letter, len2segments[self.len]) for letter in self.letters])
+        return self._inclusions
+
+    def __repr__(self):
+        return self.letters
+
+class PatternSet():
+    def __init__(self, raw_str):
+        self.patterns = [Pattern(p) for p in raw_str.split()]
+
+    @property
+    def inclusions(self):
+        inclusions = reduce(lambda a,b: a+b, map(lambda p: p.inclusions, self.patterns))
+        inclusions.simplify()
+        return inclusions
+
+    def __repr__(self):
+        return f"<PatternSet {' '.join(map(lambda p: str(p), self.patterns))} >"
 
 class Entry():
     def __init__(self, raw_str):
-        self.signals = parse_patterns(raw_str.split("|")[0])
-        self.digits  = parse_patterns(raw_str.split("|")[1])
+        signals_raw, digits_raw = raw_str.split("|")
+        self.signals = PatternSet(signals_raw)
+        self.digits  = PatternSet(digits_raw)
 
-    def __repr__(self):
-        return f"{' '.join(map(str, self.signals))} | {' '.join(map(str, self.digits))}"
+def guess_equalities(inclusions):
+    print(f"guess_equalities")
+    print(f"{inclusions.possible_eqs=}")
+    print(inclusions)
+    input()
+    for eq in inclusions.possible_eqs:
+        print(f"Insert eq {eq}")
+        inclusions_copy = deepcopy(inclusions)
+        inclusions_copy.insert_eq(eq)
+        inclusions_copy.simplify()
+        print(inclusions_copy)
+        guess_equalities(inclusions_copy)
+        print("E")
 
-def count_easy_digits(pattern_list):
-    result = 0
-    for pattern in pattern_list:
-        if pattern.len in [2, 3, 4, 7]:
-            pattern.marked = True
-            result += 1
-    return result
-
+def part2(entries):
+    entry = Entry("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+    patterns = entry.signals
+    inclusions = patterns.inclusions
+    guess_equalities(inclusions)
+        
 def parse_input(filename):
     with open(filename, "r") as f:
         lines = f.readlines()
@@ -46,11 +147,5 @@ def parse_input(filename):
     return entries
 
 
-def part1(entries):
-    count = sum(map(lambda entry: count_easy_digits(entry.digits), entries))
-    for entry in entries:
-        print(entry)
-    print(count)
-
-entries = parse_input("input")
-part1(entries)
+entries = parse_input("sample")
+part2(entries)
---
+++ solution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+        
+def parse_input(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    return lines
+
+def part1():
+    raise NotImplementedError
+
+def part2():
+    raise NotImplementedError
+
+entries = parse_input("input")
+part1()
+part2()
